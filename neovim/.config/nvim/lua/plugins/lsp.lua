bd873d7ea1e2741efa42cd1890083a13712b694f
Daniel Schaaff <daniel@danielschaaff.com>
2023-07-25 11:38:49 -0700

    update yaml config

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,10 @@
 return {
   "neovim/nvim-lspconfig",
   enabled = not vim.g.vscode,
+  dependencies = {
+    "b0o/SchemaStore.nvim",
+    version = false, -- last release is way too old
+  },
   opts = {
     servers = {
       ansiblels = {},
@@ -9,10 +13,23 @@ return {
       intelephense = {},
       tflint = {},
       yamlls = {
+        -- lazy-load schemastore when needed
+        on_new_config = function(new_config)
+          new_config.settings.yaml.schemas = new_config.settings.yaml.schemas or {}
+          vim.list_extend(new_config.settings.yaml.schemas, require("schemastore").yaml.schemas())
+        end,
         settings = {
           yaml = {
             customTags = {
               "!reference sequence",
+            },
+            format = {
+              enable = true,
+            },
+            validate = {
+              -- Must disable built-in schemaStore support to use
+              -- schemas from SchemaStore.nvim plugin
+              enable = false,
             },
             schemaStore = {
               enable = true,
