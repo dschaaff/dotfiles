bf11599ba0da322bc94d6d1cd8b7f626056c63d1
Daniel Schaaff <daniel@danielschaaff.com>
2023-12-01 09:48:08 -0800

    set wezterm to use webgpu rendering
    fonts look way better on macos
    
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
+local act = wezterm.action
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
@@ -11,15 +12,17 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
+config.front_end = "WebGpu"
 config.font_size = 12.2
-config.bold_brightens_ansi_colors = true
-config.freetype_load_target = "Light"
-config.freetype_render_target = "HorizontalLcd"
-config.freetype_load_flags = "NO_HINTING" -- seems to help rendering on high dpi screens
-config.cell_width = 0.95
+-- config.bold_brightens_ansi_colors = true
+-- config.freetype_load_target = "Light"
+-- config.freetype_render_target = "HorizontalLcd"
+-- config.freetype_load_flags = "NO_HINTING" -- seems to help rendering on high dpi screens
+-- config.cell_width = 0.95
 config.color_scheme = "tokyonight"
 config.scrollback_lines = 10000
 config.use_fancy_tab_bar = true
+config.enable_scroll_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 -- config.window_frame = {
 -- 	font = wezterm.font({ family = "Hack Nerd Font", weight = "Regular" }),
@@ -42,6 +45,25 @@ config.keys = {
 	},
 	{ key = "[", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Left") },
 	{ key = "]", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Right") },
+}
+
+config.mouse_bindings = {
+	-- Change the default click behavior so that it only selects
+	-- text and doesn't open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.CompleteSelection("ClipboardAndPrimarySelection"),
+	},
+
+	-- and make CTRL-Click open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CMD",
+		action = act.OpenLinkAtMouseCursor,
+	},
+	-- NOTE that binding only the 'Up' event can give unexpected behaviors.
+	-- Read more below on the gotcha of binding an 'Up' event only.
 }
 
 -- and finally, return the configuration to wezterm
