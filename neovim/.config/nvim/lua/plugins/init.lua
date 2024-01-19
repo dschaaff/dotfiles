e149d122c13973f8a731ec3b1c3588bf63518893
Daniel Schaaff <daniel@danielschaaff.com>
2024-01-19 08:45:00 -0800

    add fugitive gitlab plugin
    lets me use Gbrowse with gitlab

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -4,6 +4,10 @@ return {
    'tpope/vim-fugitive',
     enabled = not vim.g.vscode,
   },
+	{
+   'shumphrey/fugitive-gitlab.vim',
+    enabled = not vim.g.vscode,
+  },
 	{ 'tpope/vim-rhubarb',
     enabled = not vim.g.vscode,
   },
