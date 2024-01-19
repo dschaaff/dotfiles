7ac8cbf14cdb194aabc08fc07949ff765088e5e2
Daniel Schaaff <daniel@danielschaaff.com>
2024-01-19 08:23:43 -0800

    use relative path for file in lualine

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -23,6 +23,29 @@ return {
       component_separators = '|',
       section_separators = '',
     },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = {
+        {
+          'filename',
+          file_status = true,
+          newfile_status = false,
+          path = 1
+        }
+      },
+      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' }
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'filename' },
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {}
+    },
     extensions = { "neo-tree", "lazy" },
   },
 }
