310fd57ed6ca346da6111a10ad7eb605b7583ee1
Daniel Schaaff <daniel@danielschaaff.com>
2024-01-18 18:09:39 -0800

    fix custom tags on yaml lsp

--- neovim/.config/nvim/lua/lsp-config.lua
+++ neovim/.config/nvim/lua/lsp-config.lua
@@ -1,64 +1,64 @@
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-	-- NOTE: Remember that lua is a real programming language, and as such it is possible
-	-- to define small helper and utility functions so you don't have to repeat yourself
-	-- many times.
-	--
-	-- In this case, we create a function that lets us more easily define mappings specific
-	-- for LSP related items. It sets the mode, buffer and description for us each time.
-	local nmap = function(keys, func, desc)
-		if desc then
-			desc = 'LSP: ' .. desc
-		end
+  -- NOTE: Remember that lua is a real programming language, and as such it is possible
+  -- to define small helper and utility functions so you don't have to repeat yourself
+  -- many times.
+  --
+  -- In this case, we create a function that lets us more easily define mappings specific
+  -- for LSP related items. It sets the mode, buffer and description for us each time.
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
 
-		vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-	end
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
 
-	nmap('<leader>rn', vim.lsp.buf.rename, 'Rename')
-	nmap('<leader>ca', vim.lsp.buf.code_action, 'Code Action')
+  nmap('<leader>rn', vim.lsp.buf.rename, 'Rename')
+  nmap('<leader>ca', vim.lsp.buf.code_action, 'Code Action')
 
-	nmap('gd', require('telescope.builtin').lsp_definitions, 'Goto Definition')
-	nmap('gr', require('telescope.builtin').lsp_references, 'Goto References')
-	nmap('gI', require('telescope.builtin').lsp_implementations, 'Goto Implementation')
-	nmap('gy', require('telescope.builtin').lsp_type_definitions, 'Type Definition')
-	nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, 'Document Symbols')
-	nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Workspace Symbols')
+  nmap('gd', require('telescope.builtin').lsp_definitions, 'Goto Definition')
+  nmap('gr', require('telescope.builtin').lsp_references, 'Goto References')
+  nmap('gI', require('telescope.builtin').lsp_implementations, 'Goto Implementation')
+  nmap('gy', require('telescope.builtin').lsp_type_definitions, 'Type Definition')
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, 'Document Symbols')
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Workspace Symbols')
 
-	-- See `:help K` for why this keymap
-	nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+  -- See `:help K` for why this keymap
+  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
-	-- Lesser used LSP functionality
-	nmap('gD', vim.lsp.buf.declaration, 'Goto Declaration')
-	nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, 'Workspace Add Folder')
-	nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, 'Workspace Remove Folder')
-	nmap('<leader>wl', function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, 'Workspace List Folders')
+  -- Lesser used LSP functionality
+  nmap('gD', vim.lsp.buf.declaration, 'Goto Declaration')
+  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, 'Workspace Add Folder')
+  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, 'Workspace Remove Folder')
+  nmap('<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, 'Workspace List Folders')
 
-	-- Create a command `:Format` local to the LSP buffer
-	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-		vim.lsp.buf.format()
-	end, { desc = 'Format current buffer with LSP' })
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    vim.lsp.buf.format()
+  end, { desc = 'Format current buffer with LSP' })
 end
 
 -- document existing key chains
 require('which-key').register {
-	['<leader>c'] = { name = 'Code', _ = 'which_key_ignore' },
-	['<leader>d'] = { name = 'Document', _ = 'which_key_ignore' },
-	['<leader>g'] = { name = 'Git', _ = 'which_key_ignore' },
-	['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-	['<leader>r'] = { name = 'Rename', _ = 'which_key_ignore' },
-	['<leader>s'] = { name = 'Search', _ = 'which_key_ignore' },
-	['<leader>t'] = { name = 'Toggle', _ = 'which_key_ignore' },
-	['<leader>w'] = { name = 'Workspace', _ = 'which_key_ignore' },
+  ['<leader>c'] = { name = 'Code', _ = 'which_key_ignore' },
+  ['<leader>d'] = { name = 'Document', _ = 'which_key_ignore' },
+  ['<leader>g'] = { name = 'Git', _ = 'which_key_ignore' },
+  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+  ['<leader>r'] = { name = 'Rename', _ = 'which_key_ignore' },
+  ['<leader>s'] = { name = 'Search', _ = 'which_key_ignore' },
+  ['<leader>t'] = { name = 'Toggle', _ = 'which_key_ignore' },
+  ['<leader>w'] = { name = 'Workspace', _ = 'which_key_ignore' },
 }
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
 require('which-key').register({
-	['<leader>'] = { name = 'VISUAL <leader>' },
-	['<leader>h'] = { 'Git [H]unk' },
+  ['<leader>'] = { name = 'VISUAL <leader>' },
+  ['<leader>h'] = { 'Git [H]unk' },
 }, { mode = 'v' })
 
 -- mason-lspconfig requires that these setup functions are called in this order
@@ -75,41 +75,59 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-    ansiblels = {},
-    bashls = {},
-    dockerls = {},
-    intelephense = {},
-    tflint = {},
-    terraformls = {},
-    yamlls = {
-      keyOrdering = false,
-      format = {
-        enable = true,
-      },
-      validate = true,
-      schemaStore = {
-        -- Must disable built-in schemaStore support to use
-        -- schemas from SchemaStore.nvim plugin
-        enable = false,
-        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-        url = "",
+  ansiblels = {},
+  bashls = {},
+  dockerls = {},
+  intelephense = {},
+  jsonls = {
+    -- -- lazy-load schemastore when needed
+    -- on_new_config = function(new_config)
+    --   new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+    --   vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+    -- end,
+    settings = {
+      json = {
+        format = {
+          enable = true,
+        },
+        schemas = require('schemastore').json.schemas(),
+        validate = { enable = true },
       },
     },
-	-- clangd = {},
-	-- gopls = {},
-	-- pyright = {},
-	-- rust_analyzer = {},
-	-- tsserver = {},
-	-- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  },
+  tflint = {},
+  terraformls = {},
+  yamlls = {
+    yaml = { customTags = {'!reference sequence'}},
+    keyOrdering = false,
+    format = {
+      enable = true,
+    },
+    validate = true,
+    schemaStore = {
+      -- Must disable built-in schemaStore support to use
+      -- schemas from SchemaStore.nvim plugin
+      enable = false,
+      -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+      url = "",
+    },
+    schemas = require('schemastore').yaml.schemas(),
+  },
+  -- clangd = {},
+  -- gopls = {},
+  -- pyright = {},
+  -- rust_analyzer = {},
+  -- tsserver = {},
+  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
-	lua_ls = {
-		Lua = {
-			workspace = { checkThirdParty = false },
-			telemetry = { enable = false },
-			-- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-			-- diagnostics = { disable = { 'missing-fields' } },
-		},
-	},
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+      -- diagnostics = { disable = { 'missing-fields' } },
+    },
+  },
 }
 
 -- Setup neovim lua configuration
@@ -123,18 +141,18 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
-	ensure_installed = vim.tbl_keys(servers),
+  ensure_installed = vim.tbl_keys(servers),
 }
 
 mason_lspconfig.setup_handlers {
-	function(server_name)
-		require('lspconfig')[server_name].setup {
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = servers[server_name],
-			filetypes = (servers[server_name] or {}).filetypes,
-		}
-	end,
+  function(server_name)
+    require('lspconfig')[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+      filetypes = (servers[server_name] or {}).filetypes,
+    }
+  end,
 }
 
 -- vim: ts=2 sts=2 sw=2 et
