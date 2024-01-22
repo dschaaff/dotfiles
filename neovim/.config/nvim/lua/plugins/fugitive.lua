f7b64a311d34861719536051059c125ccba36dd6
Daniel Schaaff <daniel@danielschaaff.com>
2024-01-21 18:20:13 -0800

    map gg to open fugitive panel

--- neovim/.config/nvim/lua/plugins/fugitive.lua
+++ neovim/.config/nvim/lua/plugins/fugitive.lua
@@ -2,12 +2,13 @@ return {
   {
     "tpope/vim-fugitive",
     enabled = not vim.g.vscode,
+    lazy = true,
+    command = "Git",
+    keys = { { "<leader>gg", "<cmd>Git<CR>", mode = "n", desc = "Open Fugitive" } },
   },
-	{
-   'shumphrey/fugitive-gitlab.vim',
+  {
+    "shumphrey/fugitive-gitlab.vim",
     enabled = not vim.g.vscode,
   },
-	{ 'tpope/vim-rhubarb',
-    enabled = not vim.g.vscode,
-  }
+  { "tpope/vim-rhubarb", enabled = not vim.g.vscode },
 }
