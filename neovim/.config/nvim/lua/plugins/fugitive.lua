017ac1f8e815575cbc69f487a9693b2b1bb805de
Daniel Schaaff <daniel@danielschaaff.com>
2024-01-24 10:32:01 -0800

    fix fugitive lazy load

--- neovim/.config/nvim/lua/plugins/fugitive.lua
+++ neovim/.config/nvim/lua/plugins/fugitive.lua
@@ -3,12 +3,16 @@ return {
     "tpope/vim-fugitive",
     enabled = not vim.g.vscode,
     lazy = true,
-    command = "Git",
+    cmd = "Git",
     keys = { { "<leader>gg", "<cmd>Git<CR>", mode = "n", desc = "Open Fugitive" } },
   },
   {
     "shumphrey/fugitive-gitlab.vim",
+    lazy = true,
     enabled = not vim.g.vscode,
   },
-  { "tpope/vim-rhubarb", enabled = not vim.g.vscode },
+  {
+    "tpope/vim-rhubarb",
+    enabled = not vim.g.vscode,
+  },
 }
