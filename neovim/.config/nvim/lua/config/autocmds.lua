c7ffce34af11f4203cb5cd4cbd249e39723dde9f
Daniel Schaaff <daniel@danielschaaff.com>
2024-01-26 15:50:04 -0800

    add neovim command to open file in finder

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -31,3 +31,8 @@ vim.api.nvim_create_autocmd("FileType", {
     end)
   end,
 })
+
+vim.api.nvim_create_user_command("Finder", function()
+  local path = vim.api.nvim_buf_get_name(0)
+  os.execute("open -R " .. path)
+end, {})
