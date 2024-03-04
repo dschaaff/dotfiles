c36ddcdcb616d02141ed37f5af552c92a735cde6
Daniel Schaaff <daniel@danielschaaff.com>
2024-03-04 08:54:54 -0800

    disable noice command line

---
+++ neovim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    cmdline = {
+      enabled = false,
+      view = "cmdline",
+    },
+    -- https://github.com/LazyVim/LazyVim/discussions/1275
+    messages = {
+      enabled = false,
+    },
+    popupmenu = {
+      enabled = false,
+    },
+  },
+}
