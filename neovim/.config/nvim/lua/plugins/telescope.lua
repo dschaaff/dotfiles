a13a2b09fb791c927d1d043f2a1f14b74757bd81
Daniel Schaaff <daniel@danielschaaff.com>
2024-03-14 10:02:19 -0700

    set telescope live grep to include hidden files

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,29 +1,16 @@
 return {
   "telescope.nvim",
   enabled = not vim.g.vscode,
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
-  },
   keys = {
     { "<leader>gb", "<cmd>Telescope git_branches<CR>", desc = "branches" },
   },
   opts = {
-    defaults = {
-      vimgrep_arguments = {
-        "rg",
-        "--hidden",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-        "--glob",
-        "!.git",
+    pickers = {
+      grep_string = {
+        additional_args = { "--hidden" },
+      },
+      live_grep = {
+        additional_args = { "--hidden" },
       },
     },
   },
