d894770b606ca363d4765b4e8e2ecda803e8cbe5
Daniel Schaaff <daniel@danielschaaff.com>
2024-05-03 12:49:46 -0700

    show full file path in lualine

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -3,35 +3,18 @@ return {
   -- stole some of this from lazyvim
   "nvim-lualine/lualine.nvim",
   enabled = not vim.g.vscode,
-  opts = {
-    options = {
+  opts = function(_, opts)
+    opts.options = {
       component_separators = "|",
       section_separators = "",
-    },
-    -- sections = {
-    --   lualine_a = { "mode" },
-    --   lualine_b = { "branch", "diff", "diagnostics" },
-    lualine_c = {
-      {
-        "filename",
-        file_status = true,
-        newfile_status = false,
-        path = 1,
-      },
-    },
-    --   lualine_x = { "encoding", "fileformat", "filetype" },
-    --   lualine_y = { "progress" },
-    --   lualine_z = { "location" },
-    -- },
-    -- inactive_sections = {
-    --   lualine_a = {},
-    --   lualine_b = {},
-    --   lualine_c = { "filename" },
-    --   lualine_x = { "location" },
-    --   lualine_y = {},
-    --   lualine_z = {},
-    -- },
-    extensions = { "fugitive", "neo-tree", "lazy" },
-  },
+    }
+    opts.extensions = { "fugitive", "neo-tree", "lazy" }
+    table.remove(opts.sections.lualine_c)
+    table.insert(opts.sections.lualine_c, {
+      "filename",
+      file_status = true,
+      path = 3,
+    })
+  end,
 }
 -- vim: ts=2 sts=2 sw=2 et
