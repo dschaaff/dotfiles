411e041c9fdae9c11a7f15ab6021e35ca8633c5a
Daniel Schaaff <daniel@danielschaaff.com>
2024-12-10 10:16:45 -0800

    switch to blink for completion

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.coding.blink" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -1,16 +1,16 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "saadparwaiz1/cmp_luasnip",
-  },
-  opts = function(_, opts)
-    local cmp = require("cmp")
-    opts.mapping = cmp.mapping.preset.insert({
-      -- ctrl-y to accept completion
-      ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-    })
-  end,
+  -- "hrsh7th/nvim-cmp",
+  -- dependencies = {
+  --   "hrsh7th/cmp-nvim-lsp",
+  --   "hrsh7th/cmp-buffer",
+  --   "hrsh7th/cmp-path",
+  --   "saadparwaiz1/cmp_luasnip",
+  -- },
+  -- opts = function(_, opts)
+  --   local cmp = require("cmp")
+  --   opts.mapping = cmp.mapping.preset.insert({
+  --     -- ctrl-y to accept completion
+  --     ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+  --   })
+  -- end,
 }
