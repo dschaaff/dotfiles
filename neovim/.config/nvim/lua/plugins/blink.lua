e3d84da0aa64a4e1eee11febed708c4d7bcebe5b
Daniel Schaaff <daniel@danielschaaff.com>
2024-12-11 09:11:03 -0800

    update codecompanion config to work with blink

--- neovim/.config/nvim/lua/plugins/blink.lua
+++ neovim/.config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,21 @@
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    "olimorris/codecompanion.nvim",
+  },
   opts = {
     keymap = { preset = "default" },
+    sources = {
+      completion = {
+        enabled_providers = { "codecompanion" },
+      },
+      providers = {
+        codecompanion = {
+          name = "CodeCompanion",
+          module = "codecompanion.providers.completion.blink",
+          enabled = true,
+        },
+      },
+    },
   },
 }
