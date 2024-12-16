b516595407b6f47402f2e34e1f9c2e1d939c0123
Daniel Schaaff <daniel@danielschaaff.com>
2024-12-16 09:45:23 -0800

    switch codecompanion keymap to toggle open/close

--- neovim/.config/nvim/lua/plugins/codecompanion.lua
+++ neovim/.config/nvim/lua/plugins/codecompanion.lua
@@ -13,7 +13,7 @@ return {
     keys = {
       { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
       { "<leader>ap", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Prompt Actions (CodeCompanion)" },
-      { "<leader>aa", "<cmd>CodeCompanionChat<cr>", mode = { "n", "v" }, desc = "Open CodeCompanion" },
+      { "<leader>aa", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "Toggle (CodeCompanion)" },
       { "<leader>ac", "<cmd>CodeCompanionAdd<cr>", mode = "v", desc = "Add code to CodeCompanion" },
       { "<leader>ai", "<cmd>CodeCompanion<cr>", mode = "n", desc = "Inline prompt (CodeCompanion)" },
     },
