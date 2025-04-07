c44e851b62a343601dbb2c933bfcc2229941a7cf
Daniel Schaaff <daniel@danielschaaff.com>
2025-04-07 13:20:13 -0700

    use a different workaround for copilot.lua and lualine issue
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -1,24 +1,21 @@
 return {
   {
     "zbirenbaum/copilot.lua",
-    opts = function()
-      require("copilot.api").status = require("copilot.status")
-    end,
-    -- opts = {
-    --   filetypes = {
-    --     helm = true,
-    --     yaml = true,
-    --     terraform = true,
-    --     markdown = true,
-    --     help = false,
-    --     gitcommit = false,
-    --     gitrebase = false,
-    --     hgcommit = false,
-    --     svn = false,
-    --     cvs = false,
-    --     ["."] = false,
-    --   },
-    --   copilot_model = "gpt-4o-copilot", -- Current LSP default is gpt-35-turbo, supports gpt-4o-copilot
-    -- },
+    opts = {
+      filetypes = {
+        helm = true,
+        yaml = true,
+        terraform = true,
+        markdown = true,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_model = "gpt-4o-copilot", -- Current LSP default is gpt-35-turbo, supports gpt-4o-copilot
+    },
   },
 }
--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -15,6 +15,13 @@ return {
       file_status = true,
       path = 3,
     })
+    opts.sections.lualine_x[2] = LazyVim.lualine.status(LazyVim.config.icons.kinds.Copilot, function()
+      local clients = package.loaded["copilot"] and LazyVim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
+      if #clients > 0 then
+        local status = require("copilot.status").data.status
+        return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
+      end
+    end)
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
