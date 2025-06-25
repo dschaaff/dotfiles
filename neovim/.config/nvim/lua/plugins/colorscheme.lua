dacbc5503f52d6010ada2d437c71829579713133
Daniel Schaaff <daniel@danielschaaff.com>
2025-06-25 09:51:11 -0700

    try out night variant of tokyo night theme
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,10 @@
 return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
+  },
+
   -- { "catppuccin/nvim", name = "catppuccin", enabled = not vim.g.vscode },
 
   --   {
