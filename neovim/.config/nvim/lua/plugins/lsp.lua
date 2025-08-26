d1e52d308eb33e99abfd4a7af72ba5364291167a
Daniel Schaaff <daniel@danielschaaff.com>
2025-08-26 14:18:52 -0700

    add some python lsps
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -240,16 +240,6 @@ return {
         },
         tflint = {},
         gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
 
         lua_ls = {
           -- cmd = { ... },
@@ -267,7 +257,17 @@ return {
         },
         ['markdownlint-cli2'] = {},
         ['markdown-toc'] = {},
-
+        pyright = {},
+        basedpyright = {},
+        ruff = {
+          cmd_env = { RUFF_TRACE = 'messages' },
+          init_options = {
+            settings = {
+              logLevel = 'error',
+            },
+          },
+        },
+        ['ruff-lsp'] = {},
         vale = {},
         markdownlint = {},
         shellcheck = {},
