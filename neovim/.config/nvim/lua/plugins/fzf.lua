6eb1c1c45f9c1cd3ed847775ec17e7454eb1d6ad
Daniel Schaaff <daniel@danielschaaff.com>
2025-08-26 13:42:38 -0700

    add some useful keymaps for fzf lua
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

--- neovim/.config/nvim/lua/plugins/fzf.lua
+++ neovim/.config/nvim/lua/plugins/fzf.lua
@@ -2,6 +2,18 @@ return {
   'ibhagwan/fzf-lua',
   enabled = not vim.g.vscode,
   cmd = 'FzfLua',
+  config = function()
+    local config = require('fzf-lua.config')
+    -- Quickfix
+    config.defaults.keymap.fzf["ctrl-q"] = "select-all+accept"
+    config.defaults.keymap.fzf["ctrl-u"] = "half-page-up"
+    config.defaults.keymap.fzf["ctrl-d"] = "half-page-down"
+    config.defaults.keymap.fzf["ctrl-x"] = "jump"
+    config.defaults.keymap.fzf["ctrl-f"] = "preview-page-down"
+    config.defaults.keymap.fzf["ctrl-b"] = "preview-page-up"
+    config.defaults.keymap.builtin["<c-f>"] = "preview-page-down"
+    config.defaults.keymap.builtin["<c-b>"] = "preview-page-up"
+  end,
   keys = {
     { '<c-j>', '<c-j>', ft = 'fzf', mode = 't', nowait = true },
     { '<c-k>', '<c-k>', ft = 'fzf', mode = 't', nowait = true },
