ecb938af4f6ab8f81b15fd360f6980c87f75401d
Daniel Schaaff <daniel@danielschaaff.com>
2025-08-26 14:22:19 -0700

    update yaml settings
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -282,9 +282,18 @@ return {
           settings = {
             redhat = { telemetry = { enabled = false } },
             yaml = {
+              keyOrdering = false,
+              format = {
+                enable = true,
+              },
+              validate = true,
               customTags = { '!reference sequence' },
               schemaStore = {
-                enable = true,
+                -- Must disable built-in schemaStore support to use
+                -- schemas from SchemaStore.nvim plugin
+                enable = false,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = '',
               },
             },
           },
