47f91828734399846ee71c5ffcd8e3db3291de30
Daniel Schaaff <daniel@danielschaaff.com>
2025-08-27 08:51:23 -0700

    set neotree to follow the currently open file
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

--- neovim/.config/nvim/lua/plugins/neotree.lua
+++ neovim/.config/nvim/lua/plugins/neotree.lua
@@ -12,7 +12,10 @@ return {
     { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree [E]xplorer Toggle', silent = true },
   },
   opts = {
+    open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
     filesystem = {
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
       window = {
         mappings = {
           ['\\'] = 'close_window',
