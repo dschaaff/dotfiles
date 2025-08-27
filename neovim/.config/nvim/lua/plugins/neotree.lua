1c85b230efac59421092dbd2b1ce13d4fe6a2c81
Daniel Schaaff <daniel@danielschaaff.com>
2025-08-27 08:59:21 -0700

    add lsp file operations plugin for neotree
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

--- neovim/.config/nvim/lua/plugins/neotree.lua
+++ neovim/.config/nvim/lua/plugins/neotree.lua
@@ -1,26 +1,38 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  enabled = not vim.g.vscode,
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  lazy = false,
-  keys = {
-    { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree [E]xplorer Toggle', silent = true },
-  },
-  opts = {
-    open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
-    filesystem = {
-      follow_current_file = { enabled = true },
-      use_libuv_file_watcher = true,
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    enabled = not vim.g.vscode,
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+    lazy = false,
+    keys = {
+      { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree [E]xplorer Toggle', silent = true },
+    },
+    opts = {
+      open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
+      filesystem = {
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+          },
         },
       },
     },
+  },
+  {
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-neo-tree/neo-tree.nvim', -- makes sure that this loads after Neo-tree.
+    },
+    config = function()
+      require('lsp-file-operations').setup()
+    end,
   },
 }
