37ee60b840831df6bcbd2a864b5a472df13114d5
Daniel Schaaff <daniel@danielschaaff.com>
2025-09-02 14:27:49 -0700

    add diffview.nvim

--- neovim/.config/nvim/lua/plugins/diffview.lua
+++ neovim/.config/nvim/lua/plugins/diffview.lua
@@ -1,30 +1,5 @@
 return {
-  'MagicDuck/grug-far.nvim',
-  opts = {
-    headerMaxWidth = 80,
-    engines = {
-      ripgrep = {
-        extraArgs = '--hidden --glob !.git/ --glob !.terraform --glob !node_modules --glob !.DS_Store',
-      },
-    },
-  },
-  cmd = 'GrugFar',
-  keys = {
-    {
-      '<leader>sr',
-      function()
-        local grug = require('grug-far')
-        local ext = vim.bo.buftype == '' and vim.fn.expand('%:e')
-        grug.open({
-          transient = true,
-          prefills = {
-            filesFilter = ext and ext ~= '' and '*.' .. ext or nil,
-          },
-        })
-      end,
-      mode = { 'n', 'v' },
-      desc = 'Search and Replace',
-    },
-  },
+  'sindrets/diffview.nvim',
+  cmd = { 'DiffviewOpen', 'DiffviewFileHistory', 'Diffview' },
 }
 -- vim: ts=2 sts=2 sw=2 et
