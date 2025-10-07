da2218cd183b25b5c0a150934056d8cb803ed8d1
Daniel Schaaff <daniel@danielschaaff.com>
2025-10-07 09:16:40 -0700

    switch to snacks lazygit package
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

---
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  'folke/snacks.nvim',
+  opts = {
+    -- lazygit = {},
+  },
+  keys = {
+    {
+      '<leader>gg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'LazyGit',
+    },
+  },
+}
