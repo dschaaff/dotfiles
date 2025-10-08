bd217d180b2dbdd6989ef340ee3ee68f73aefa3f
Daniel Schaaff <daniel@danielschaaff.com>
2025-10-08 09:39:32 -0700

    add sidekick.nvim for running claude code directly in nvim
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

---
+++ neovim/.config/nvim/lua/plugins/sidekick.lua
@@ -0,0 +1,68 @@
+return {
+  'folke/sidekick.nvim',
+  opts = {
+    -- add any options here
+    -- cli = {
+    --   mux = {
+    --     backend = "zellij",
+    --     enabled = true,
+    --   },
+    -- },
+    nes = {
+      enabled = false,
+    },
+  },
+  keys = {
+    --   {
+    --     '<tab>',
+    --     function()
+    --       -- if there is a next edit, jump to it, otherwise apply it if any
+    --       if not require('sidekick').nes_jump_or_apply() then
+    --         return '<Tab>' -- fallback to normal tab
+    --       end
+    --     end,
+    --     expr = true,
+    --     desc = 'Goto/Apply Next Edit Suggestion',
+    --   },
+    --   {
+    --     '<c-.>',
+    --     function()
+    --       require('sidekick.cli').focus()
+    --     end,
+    --     desc = 'Sidekick Switch Focus',
+    --     mode = { 'n', 'v' },
+    --   },
+    --   {
+    --     '<leader>aa',
+    --     function()
+    --       require('sidekick.cli').toggle({ focus = true })
+    --     end,
+    --     desc = 'Sidekick Toggle CLI',
+    --     mode = { 'n', 'v' },
+    --   },
+    {
+      '<leader>acc',
+      function()
+        require('sidekick.cli').toggle({ name = 'claude', focus = true })
+      end,
+      desc = 'Sidekick Claude Toggle',
+      mode = { 'n', 'v' },
+    },
+    {
+      '<leader>acg',
+      function()
+        require('sidekick.cli').toggle({ name = 'gemini', focus = true })
+      end,
+      desc = 'Sidekick Grok Toggle',
+      mode = { 'n', 'v' },
+    },
+    --   {
+    --     '<leader>ap',
+    --     function()
+    --       require('sidekick.cli').select_prompt()
+    --     end,
+    --     desc = 'Sidekick Ask Prompt',
+    --     mode = { 'n', 'v' },
+    --   },
+  },
+}
