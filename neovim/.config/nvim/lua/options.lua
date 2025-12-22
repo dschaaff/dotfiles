5736aa055c2ea61e9d53f7be33d0e722f3c1c33a
Daniel Schaaff <daniel@danielschaaff.com>
2025-12-22 13:39:38 -0800

    set env var to fix cluade not using bedrock when launched in sidekick.nvim
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

--- neovim/.config/nvim/lua/options.lua
+++ neovim/.config/nvim/lua/options.lua
@@ -113,4 +113,7 @@ end
 -- The LSP logfile continually grows in size when enabled
 -- only enable when need to debug
 vim.lsp.set_log_level('off')
+
+-- Use bedrock in claude code, launched via sidekick
+vim.env.CLAUDE_CODE_USE_BEDROCK = '1'
 -- vim: ts=2 sts=2 sw=2 et
