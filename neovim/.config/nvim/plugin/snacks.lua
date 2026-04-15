ec3ae7c443a33f1e19bb302db061f0c6818edeac
Daniel Schaaff <daniel@danielschaaff.com>
2026-04-15 11:04:16 -0700

    use snacks for git browse
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

--- neovim/.config/nvim/plugin/snacks.lua
+++ neovim/.config/nvim/plugin/snacks.lua
@@ -1,8 +1,13 @@
 ---@type snacks.Config
 require('snacks').setup({
+  gitbrowse = { enabled = true },
   lazygit = { enabled = true },
 })
 
 vim.keymap.set('n', '<leader>gg', function()
   require('snacks').lazygit()
 end, { desc = 'LazyGit' })
+
+vim.keymap.set({ 'n', 'v' }, '<leader>gB', function()
+  require('snacks').gitbrowse()
+end, { desc = 'Open in browser' })
