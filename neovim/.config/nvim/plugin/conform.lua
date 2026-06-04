90b50f03d555a6b6e32f913b9346315eb0ac71db
Daniel Schaaff <daniel@danielschaaff.com>
2026-06-04 10:22:01 -0700

    just use oxfmt for formatting js
    Signed-off-by: Daniel Schaaff <daniel@danielschaaff.com>

--- neovim/.config/nvim/plugin/conform.lua
+++ neovim/.config/nvim/plugin/conform.lua
@@ -16,21 +16,16 @@ require('conform').setup({
     sh = { 'shfmt' },
     markdown = { 'prettierd', 'prettier', stop_after_first = true },
     python = { 'ruff_organize_imports', 'ruff_format' },
-    javascript = { 'oxlint_fix', 'oxfmt' },
-    javascriptreact = { 'oxlint_fix', 'oxfmt' },
-    typescript = { 'oxlint_fix', 'oxfmt' },
-    typescriptreact = { 'oxlint_fix', 'oxfmt' },
+    javascript = { 'oxfmt' },
+    javascriptreact = { 'oxfmt' },
+    typescript = { 'oxfmt' },
+    typescriptreact = { 'oxfmt' },
   },
   formatters = {
     oxfmt = {
       command = 'oxfmt',
       args = { '--stdin-filename', '$FILENAME' },
       stdin = true,
-    },
-    oxlint_fix = {
-      command = 'oxlint',
-      args = { '--fix', '$FILENAME' },
-      stdin = false,
     },
   },
 })
